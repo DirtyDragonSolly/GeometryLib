00cb0f625b4ba3f1ec60200db0cd393fdd65fd0b
Ilya <drtdrgnsolly@mail.ru>
2024-09-04 22:29:32 +0300

    Initial commit

---
+++ GeometryLib/GeometryLib/Shapes.cs
@@ -0,0 +1,59 @@
+﻿namespace GeometryLib
+{
+    public class Circle : IShape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        public double SideA { get; }
+        public double SideB { get; }
+        public double SideC { get; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+
+            if (!IsValidTriangle())
+            {
+                throw new ArgumentException("Invalid triangle sides");
+            }
+        }
+
+        public double CalculateArea()
+        {
+            double semiPerimeter = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(semiPerimeter *
+                             (semiPerimeter - SideA) *
+                             (semiPerimeter - SideB) *
+                             (semiPerimeter - SideC));
+        }
+
+        public bool IsRightTriangle()
+        {
+            var sides = new[] { SideA, SideB, SideC };
+            Array.Sort(sides);
+            return Math.Abs(Math.Pow(sides[2], 2) - (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2))) < 1e-10;
+        }
+
+        private bool IsValidTriangle()
+        {
+            return SideA + SideB > SideC &&
+                   SideA + SideC > SideB &&
+                   SideB + SideC > SideA;
+        }
+    }
+}
